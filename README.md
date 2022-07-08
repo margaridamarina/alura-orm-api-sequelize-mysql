56ab91300739eaa5b8fc77f0b93faa01d050e7f1
Margarida <margaridammcm@gmail.com>
2022-07-07 22:07:34 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### Criando arquivo de configuração
+```
+$ npm init -y
+```
+### Instalando framework para ajudar no desenvolvimento rapido, minimalista e nao opinativo da api
+```
+$ npm install express 
+```
+### Instalando biblioteca para converter dados
+```
+$ npm install body-parser
+```
+### Rodar programa
+```
+$ node api/index.js
+```
---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const bodyParser = require ('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const port = 4000
+
+app.get('/teste', (req, res) => res
+  .status(200)
+  .send({messagem: 'Boas-vindas à API!'
+}))
+
+app.listen(port, () => console.log(`O servidor está rodando na porta ${port}`))
+
+module.exports = app
