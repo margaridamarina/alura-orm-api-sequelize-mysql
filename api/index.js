ed031c93ad04bab66f55e7e2bfac57fffe35a8aa
Margarida <margaridammcm@gmail.com>
2022-07-10 15:42:36 -0300

    Definindo rota pessoas

--- api/index.js
+++ api/index.js
@@ -1,16 +1,10 @@
 const express = require ('express')
-const bodyParser = require ('body-parser')
+const routes = require ('./routes')
 
 const app = express()
-
-app.use(bodyParser.json())
-
 const port = 4000
 
-app.get('/teste', (req, res) => res
-  .status(200)
-  .send({messagem: 'Boas-vindas à API!'
-}))
+routes(app)
 
 app.listen(port, () => console.log(`O servidor está rodando na porta ${port}`))
 
