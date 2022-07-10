4887a328f032678c73c9553778ca899ca3d543b6
Margarida <margaridammcm@gmail.com>
2022-07-10 15:48:50 -0300

    definindo rota para listar pessoas por id

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -8,7 +8,20 @@ class PessoaController {
     } catch (error) {
       return res.status(500).json(error.message)
     }
-    
+  }
+
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params 
+    try {
+      const umaPessoa = await database.Pessoas.findOne( { 
+        where: { 
+          id: Number(id) 
+        } 
+      })
+      return res.status(200).json(umaPessoa)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
   }
 }
 
